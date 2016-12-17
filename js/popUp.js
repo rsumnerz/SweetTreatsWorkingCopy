fc659802781038f117c4f9e40ecf7d1d4c2eb994
Esterling Accime <accimeesterlin@yahoo.com>
2016-12-17 11:53:33 -0500

    Gallery pops up

--- js/popUp.js
+++ js/popUp.js
@@ -1,8 +1,22 @@
-// $('.popup').css('width', '20rem');
 
 
-$('.popup').on('click', function(){
-	// add the class test to add the style
-	$('.thumbnail').toggleClass('test');
-	// toggle the class so it can on and off
-});
+var thumbnail = function(element){
+	// if the element does or doesn't have a class of those below, then add the class, if yes, then remove the class
+
+	$(element).toggleClass('test col-xs-6 col-md-3');
+
+	// Fixing Bootstrap margin left and right bug
+	$('.container').toggleClass('container');
+	// $('a').toggleClass('thumbnail');
+
+
+    // Styling the right image
+	if($(element).hasClass('test')){
+		$(".test a img").addClass('locating');
+	}
+
+	// else, then removing
+	else{
+		$('img').removeClass('locating');
+	}
+};
