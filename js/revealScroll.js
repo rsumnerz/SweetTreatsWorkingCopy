99446f6799f93b870827e9985f72623bdda956b3
AnnMarie <AnnMarieTorres@outlook.com>
2016-12-30 13:25:02 -0500

    scroll effect on about us and cake.html

--- js/revealScroll.js
+++ js/revealScroll.js
@@ -10,5 +10,115 @@ $revealServices.waypoint(function (direction) {
 	}else{
 		$revealServices.removeClass('js-animate');
 	}
-},{offset:'75%'});
+},{offset:'85%'});
 
+var $revealServices1 = $('.reveal-services1');
+
+$revealServices1.waypoint(function (direction) {
+	if(direction =='down') {
+		$revealServices1.addClass('js-animate');
+	}else{
+		$revealServices1.removeClass('js-animate');
+	}
+},{offset:'85%'});
+
+var $revealServices2 = $('.reveal-services2');
+
+$revealServices2.waypoint(function (direction) {
+	if(direction =='down') {
+		$revealServices2.addClass('js-animate');
+	}else{
+		$revealServices2.removeClass('js-animate');
+	}
+},{offset:'85%'});
+var $revealServices3 = $('.reveal-services3');
+
+$revealServices3.waypoint(function (direction) {
+	if(direction =='down') {
+		$revealServices3.addClass('js-animate');
+	}else{
+		$revealServices3.removeClass('js-animate');
+	}
+},{offset:'85%'});
+
+var $revealServices4 = $('.reveal-services4');
+
+$revealServices4.waypoint(function (direction) {
+	if(direction =='down') {
+		$revealServices4.addClass('js-animate');
+	}else{
+		$revealServices4.removeClass('js-animate');
+	}
+},{offset:'85%'});
+
+var $revealServices5 = $('.reveal-services5');
+
+$revealServices5.waypoint(function (direction) {
+	if(direction =='down') {
+		$revealServices5.addClass('js-animate');
+	}else{
+		$revealServices5.removeClass('js-animate');
+	}
+},{offset:'85%'});
+
+
+var $revealServices6 = $('.reveal-services6');
+
+$revealServices6.waypoint(function (direction) {
+	if(direction =='down') {
+		$revealServices6.addClass('js-animate');
+	}else{
+		$revealServices6.removeClass('js-animate');
+	}
+},{offset:'85%'});
+
+
+var $revealServices7 = $('.reveal-services7');
+
+$revealServices7.waypoint(function (direction) {
+	if(direction =='down') {
+		$revealServices7.addClass('js-animate');
+	}else{
+		$revealServices7.removeClass('js-animate');
+	}
+},{offset:'85%'});
+
+var $revealServices8 = $('.reveal-services8');
+
+$revealServices8.waypoint(function (direction) {
+	if(direction =='down') {
+		$revealServices8.addClass('js-animate');
+	}else{
+		$revealServices8.removeClass('js-animate');
+	}
+},{offset:'85%'});
+
+var $revealServices9 = $('.reveal-services9');
+
+$revealServices9.waypoint(function (direction) {
+	if(direction =='down') {
+		$revealServices9.addClass('js-animate');
+	}else{
+		$revealServices9.removeClass('js-animate');
+	}
+},{offset:'85%'});
+
+var $revealServices10 = $('.reveal-services10');
+
+$revealServices10.waypoint(function (direction) {
+	if(direction =='down') {
+		$revealServices10.addClass('js-animate');
+	}else{
+		$revealServices10.removeClass('js-animate');
+	}
+},{offset:'85%'});
+
+var $revealServices11 = $('.reveal-services11');
+
+$revealServices11.waypoint(function (direction) {
+	if(direction =='down') {
+		$revealServices11.addClass('js-animate');
+	}else{
+		$revealServices11.removeClass('js-animate');
+	}
+},{offset:'85%'});
